ceb170be9af87380ef300dc7b8aa7f882d10aab3
donghaomin <dhmcharles@gmail.com>
2014-03-22 13:42:47 +0100

    [2/2] Various status bar battery style options
    * Add native battery bar with percentage option.
    * Add circle battery (with percentage) option (Credits @Sven-Dawitz).
    * Add icon style reflected by QS battery tile (Credits @kufikugel).
    
    [2/2] 多种电池图标样式选项
    * 添加原生电池百分比选项.
    * 添加圆圈电池(+百分比)选项 (感谢 @Sven-Dawitz).
    * 添加快捷设置电池磁贴映射通知栏电池样式 (感谢 @kufikugel).
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/StatusBarSettings.java
+++ src/com/android/settings/biantai/StatusBarSettings.java
@@ -3,7 +3,9 @@ package com.android.settings.biantai;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -11,11 +13,18 @@ import android.provider.Settings.SettingNotFoundException;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class StatusBarSettings extends SettingsPreferenceFragment {
+public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    // Status bar brightness control
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+    // Status bar battery style
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
+    // Status bar brightness control
     private CheckBoxPreference mStatusBarBrightnessControl;
+    // Status bar battery style
+    private ListPreference mStatusBarBattery;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,27 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
             }
         } catch (SettingNotFoundException e) {
         }
+
+        // Status bar battery style
+        mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+
+        int batteryStyleValue = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        mStatusBarBattery.setValue(String.valueOf(batteryStyleValue));
+        mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mStatusBarBattery) {
+            int batteryStyleValue = Integer.valueOf((String) objValue);
+            int batteryStyleIndex = mStatusBarBattery.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_BATTERY_STYLE, batteryStyleValue);
+            mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[batteryStyleIndex]);
+            return true;
+        }
+        return false;
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
