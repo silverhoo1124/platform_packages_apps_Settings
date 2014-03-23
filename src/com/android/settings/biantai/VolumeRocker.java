ae886ef274d2b0fce33a521c336340c659022091
donghaomin <dhmcharles@gmail.com>
2014-03-23 09:19:05 +0100

    [2/2] Unlinked volumes & expandable volume overlay
    * Add option to unlink volumes.
    * Add user configurable options for volume overlay.
        Choose from single, expandable, expanded and hidden.
    
    [2/2] 分离音量 & 可展开音量调节面板
    
    * 添加允许独立调整各项音量的选项.
    * 添加用户可配置的音量调节弹出框.
        可在"单行", "可展开", "已展开", "隐藏"中选择.
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/VolumeRocker.java
+++ src/com/android/settings/biantai/VolumeRocker.java
@@ -42,18 +42,23 @@ package com.android.settings.biantai;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.view.VolumePanel;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class VolumeRocker extends SettingsPreferenceFragment {
+public class VolumeRocker extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
+    private static final String KEY_VOLUME_OVERLAY = "volume_overlay";
 
     private CheckBoxPreference mVolumeAdjustSounds;
+    private ListPreference mVolumeOverlay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,6 +69,14 @@ public class VolumeRocker extends SettingsPreferenceFragment {
         mVolumeAdjustSounds = (CheckBoxPreference) findPreference(KEY_VOLUME_ADJUST_SOUNDS);
         mVolumeAdjustSounds.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) != 0);
+
+        mVolumeOverlay = (ListPreference) findPreference(KEY_VOLUME_OVERLAY);
+        mVolumeOverlay.setOnPreferenceChangeListener(this);
+        int volumeOverlay = Settings.System.getInt(getContentResolver(),
+                Settings.System.MODE_VOLUME_OVERLAY,
+                VolumePanel.VOLUME_OVERLAY_EXPANDABLE);
+        mVolumeOverlay.setValue(Integer.toString(volumeOverlay));
+        mVolumeOverlay.setSummary(mVolumeOverlay.getEntry());
     }
 
     @Override
@@ -74,5 +87,17 @@ public class VolumeRocker extends SettingsPreferenceFragment {
                     mVolumeAdjustSounds.isChecked() ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mVolumeOverlay) {
+            int valueVolumeOverlay = Integer.valueOf((String) objValue);
+            int indexVolumeOverlay = mVolumeOverlay.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.MODE_VOLUME_OVERLAY, valueVolumeOverlay);
+            mVolumeOverlay.setSummary(mVolumeOverlay.getEntries()[indexVolumeOverlay]);
+            return true;
+        }
+        return true;
     }
 }
