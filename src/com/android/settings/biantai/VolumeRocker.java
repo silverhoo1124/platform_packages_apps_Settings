2005736ea256606682a3c8e33a673fed1168db4b
donghaomin <dhmcharles@gmail.com>
2014-03-23 08:03:25 +0100

    [2/2] Volume adjust sound & KitKat style drawables
    * Add volume adjust sound, the sound made when
        you adjust volume with volume rocker.
    * Add an option for user to enable/diable the sound.
    * Refresh related drawables with KitKat style ones.
    
    [2/2] 音量调整音 & KitKat风drawables
    
    * 添加音量调整音, 即当你用音量键调整声音时发出的声音.
    * 添加用户可设置的选项来启用/禁用音量调整音.
    * 换血相关的drawables为KitKat风.
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/VolumeRocker.java
+++ src/com/android/settings/biantai/VolumeRocker.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
+/*
                          $$$$$$$$$$$$$$$
                       $$$$$$        $$  $$$$
                      $$$             $     $$$
@@ -37,20 +36,43 @@
             $$$$$                    $$$$
            $$$$$$                    $$$
                                    $$$$
--->
-<PreferenceScreen
-        xmlns:android="http://schemas.android.com/apk/res/android"
-        android:title="@string/system_interface_title"
-        xmlns:settings="http://schemas.android.com/apk/res/com.android.settings">
+*/
 
-    <PreferenceScreen
-        android:fragment="com.android.settings.biantai.StatusBarSettings"
-        android:key="status_bar"
-        android:title="@string/status_bar_category_title" />
+package com.android.settings.biantai;
 
-    <PreferenceScreen
-        android:fragment="com.android.settings.biantai.VolumeRocker"
-        android:key="volume_rocker_settings"
-        android:title="@string/volume_rocker_category_title" />
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
-</PreferenceScreen>
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class VolumeRocker extends SettingsPreferenceFragment {
+
+    private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
+
+    private CheckBoxPreference mVolumeAdjustSounds;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.volume_rocker_settings);
+
+        mVolumeAdjustSounds = (CheckBoxPreference) findPreference(KEY_VOLUME_ADJUST_SOUNDS);
+        mVolumeAdjustSounds.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) != 0);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mVolumeAdjustSounds) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED,
+                    mVolumeAdjustSounds.isChecked() ? 1 : 0);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
