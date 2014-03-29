0dd65fd102e898f3f72d5728b73f208f295e15fa
donghaomin <dhmcharles@gmail.com>
2014-03-29 21:25:01 +0100

    [2/2] User configurable safe headset volume
    * Add a user configurable option to
        enable/disable safe headset volume.
    * Add restore volumes after boot and when a headset is plugged.
    
    [2/2] 用户可配置的安全耳机音量
    
    * 添加一个用户可配置的选项
        来启用/禁用安全耳机音量.
    * 添加耳机插入时, 设备启动后恢复音量.
    
    [1/2] in platform_frameworks_base
    
    Thanks to CyanogenMod for the original implementation.
    感谢CyanogenMod原创实现此功能.

--- src/com/android/settings/biantai/VolumeRocker.java
+++ src/com/android/settings/biantai/VolumeRocker.java
@@ -59,12 +59,14 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
     private CheckBoxPreference mVolumeAdjustSounds;
     private ListPreference mVolumeOverlay;
     private ListPreference mVolumeKeyCursorControl;
     private CheckBoxPreference mVolumeWake;
     private CheckBoxPreference mVolBtnMusicCtrl;
+    private CheckBoxPreference mSafeHeadsetVolume;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -99,6 +101,13 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
         mVolBtnMusicCtrl = (CheckBoxPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
         mVolBtnMusicCtrl.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.VOLBTN_MUSIC_CONTROLS, 0) == 1);
+
+        mSafeHeadsetVolume = (CheckBoxPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setPersistent(false);
+        boolean safeMediaVolumeEnabled = getResources().getBoolean(
+                com.android.internal.R.bool.config_safe_media_volume_enabled);
+        mSafeHeadsetVolume.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SAFE_HEADSET_VOLUME, safeMediaVolumeEnabled ? 1 : 0) != 0);
     }
 
     @Override
@@ -115,6 +124,10 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.VOLBTN_MUSIC_CONTROLS,
                     mVolBtnMusicCtrl.isChecked() ? 1 : 0);
+        } else if (preference == mSafeHeadsetVolume) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SAFE_HEADSET_VOLUME,
+                    mSafeHeadsetVolume.isChecked() ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
