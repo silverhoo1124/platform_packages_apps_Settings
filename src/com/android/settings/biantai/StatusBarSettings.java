a62beea027af2f4de2d1ffef8ff07c3db6538238
donghaomin <dhmcharles@gmail.com>
2014-04-18 17:31:31 +0800

    [2/2] QuickSettings quick pulldown & swipe to switch
    * Add a user configurable QuickSettings quick pulldown option.
    * Add a swipe-to-switch action between Notification/QuickSettings panel.
    
    [2/2] 快捷设置面板快速下拉 & 滑动切换
    
    * 添加一个用户可配置的快捷设置面板快速下拉选项.
    * 添加通知面板和快捷设置面板之间的滑动切换操作.
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/StatusBarSettings.java
+++ src/com/android/settings/biantai/StatusBarSettings.java
@@ -12,6 +12,7 @@ import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -19,11 +20,15 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     // Status bar battery style
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    // QuickSettings panel quick pulldown
+    private static final String QUICK_PULLDOWN = "quick_pulldown";
 
     // Status bar brightness control
     private CheckBoxPreference mStatusBarBrightnessControl;
     // Status bar battery style
     private ListPreference mStatusBarBattery;
+    // QuickSettings panel quick pulldown
+    private ListPreference mQuickPulldown;
 
 
     @Override
@@ -47,6 +52,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
         }
 
         // Status bar battery style
+        // 通知栏电池样式
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
         mStatusBarBattery.setOnPreferenceChangeListener(this);
 
@@ -54,6 +60,23 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
                 Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
         mStatusBarBattery.setValue(String.valueOf(batteryStyleValue));
         mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+
+        // QuickSettings panel quick pulldown
+        // 快捷设置面板快速下拉
+        mQuickPulldown = (ListPreference) getPreferenceScreen().findPreference(QUICK_PULLDOWN);
+        // Only phones need this feature
+        // 只有手机需要这个特性
+        if (!Utils.isPhone(getActivity())) {
+            if (mQuickPulldown != null) {
+                getPreferenceScreen().removePreference(mQuickPulldown);
+            }
+        } else {
+            mQuickPulldown.setOnPreferenceChangeListener(this);
+            int quickPulldownValue = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.QS_QUICK_PULLDOWN, 0);
+            mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+            mQuickPulldown.setSummary(mQuickPulldown.getEntry());
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -64,6 +87,15 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
                     Settings.System.STATUS_BAR_BATTERY_STYLE, batteryStyleValue);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[batteryStyleIndex]);
             return true;
+
+        } else if (preference == mQuickPulldown) {
+            int quickPulldownValue = Integer.valueOf((String) objValue);
+            int quickPulldownIndex = mQuickPulldown.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.QS_QUICK_PULLDOWN, quickPulldownValue);
+            mQuickPulldown.setSummary(mQuickPulldown.getEntries()[quickPulldownIndex]);
+            return true;
+
         }
         return false;
     }
