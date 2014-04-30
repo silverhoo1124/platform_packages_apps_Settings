1d3ccdee573e234867417aabf7f319c58d23776b
donghaomin <dhmcharles@gmail.com>
2014-04-30 10:57:53 +0800

    [2/2] Navigation bar height customization
    As the title says, this allows users to customize the height
    of navigation bar buttons.
    
    [2/2] 导航栏高度自定义
    
    如标题所述, 这允许用户自定义导航栏按键的高度.
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/NavigationBarSettings.java
+++ src/com/android/settings/biantai/NavigationBarSettings.java
@@ -41,7 +41,10 @@
 package com.android.settings.biantai;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -50,15 +53,32 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "NavigationBarSettings";
 
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+    private ListPreference mNavigationBarHeight;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.navigation_bar_settings);
+
+        mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setOnPreferenceChangeListener(this);
+        int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
+        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBarHeight) {
+            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
+            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
+            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
+        }
+        return true;
     }
 }
