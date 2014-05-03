f9aa5a448961774ac03a241c78e409090437045d
donghaomin <dhmcharles@gmail.com>
2014-05-04 01:09:43 +0800

    [2/2] Alternative screen-off animations
    Add alternative screen-off animations.
    Now users are allowed to choose from the following options:
    * Fade out
    * Electron beam[CRT screen-off] (default)
    * Scale down
    
    [2/2] 备用关屏动画
    
    添加备用关屏动画.
    现在用户可在下列选项中选取:
    * 淡出
    * 电子光束[CRT显示器关屏] (默认)
    * 收缩
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/SystemUiSettings.java
+++ src/com/android/settings/biantai/SystemUiSettings.java
@@ -40,8 +40,14 @@
 
 package com.android.settings.biantai;
 
+import static android.provider.Settings.System.SCREEN_OFF_ANIMATION;
+
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.provider.Settings;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -50,15 +56,54 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements Pref
 
     private static final String TAG = "SystemUiSettings";
 
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+    private ListPreference mScreenOffAnimationPreference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         addPreferencesFromResource(R.xml.system_ui_settings);
+
+        mScreenOffAnimationPreference = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        final int currentAnimation = Settings.System.getInt(resolver, SCREEN_OFF_ANIMATION, 1 /* CRT-off */);
+        mScreenOffAnimationPreference.setValue(String.valueOf(currentAnimation));
+        mScreenOffAnimationPreference.setOnPreferenceChangeListener(this);
+        updateScreenOffAnimationPreferenceDescription(currentAnimation);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+    private void updateScreenOffAnimationPreferenceDescription(int currentAnim) {
+        ListPreference preference = mScreenOffAnimationPreference;
+        String summary;
+        if (currentAnim < 0) {
+            // Unsupported value
+            summary = "";
+        } else {
+            final CharSequence[] entries = preference.getEntries();
+            final CharSequence[] values = preference.getEntryValues();
+            if (entries == null || entries.length == 0) {
+                summary = "";
+            } else {
+                summary = entries[currentAnim].toString();
+            }
+        }
+        preference.setSummary(summary);
     }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if (KEY_SCREEN_OFF_ANIMATION.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), SCREEN_OFF_ANIMATION, value);
+                updateScreenOffAnimationPreferenceDescription(value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist screen-off animation setting", e);
+            }
+        }
+        return true;
+    }
+
 }
